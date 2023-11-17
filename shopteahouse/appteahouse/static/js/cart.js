d65ca0e2d466d90dda29960fc2260627c0eada7c
TRAN VAN CHUONG <tranvanchuong6172@outlook.com>
2023-11-17 19:55:34 +0700

    first commit

---
+++ shopteahouse/appteahouse/static/js/cart.js
@@ -0,0 +1,33 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+for (i=0;i<updateBtns.length;i++){
+    updateBtns[i].addEventListener('click',function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId',productId,'action',action)
+        console.log('user',user)
+        if (user === "AnonymousUser"){
+            console.log('user logger in')
+        }else{
+            updateUserOrder(productId,action)
+        }
+    })
+}
+function updateUserOrder(productId,action){
+    console.log('user logger in, success add')
+    var url = '/update_item/'
+    fetch(url,{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId':productId,'action':action})
+    })
+    .then((respose) => {
+    return respose.json()
+    })
+    .then((data) => {
+        console.log('data',data)
+        location.reload()
+    })
+}
